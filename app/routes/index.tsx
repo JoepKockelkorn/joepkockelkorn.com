908c02a8fa8bfd75bf371cf46b943a8463679f96
Joep Kockelkorn <joepkockelkorn@gmail.com>
2022-10-16 14:01:36 +0200

    feat: remove medium, improve colors & sizes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,32 +2,41 @@ import * as React from 'react';
 
 export default function Index() {
   return (
-    <main className='flex flex-col min-h-full justify-center text-center py-0 px-8 bg-gray-100 text-gray-600'>
-      <div className='font-semibold text-4xl md:text-6xl'>
+    <main className='flex flex-col min-h-full justify-center text-center py-0 px-8 bg-gray-100 text-gray-800'>
+      <div className='font-semibold text-5xl'>
         Hello, I'm Joep Kockelkorn!
       </div>
-      <div className='font-mono font-thin text-2xl md:text-3xl mt-4 mx-0'>
+      <div className='font-mono text-3xl mt-4 mx-0'>
         Full Stack Dev && Front-end fanatic
       </div>
-      <div className='mt-8 flex flex-row justify-center flex-wrap'>
-        <a className='flex items-center hover:text-yellow-500 group' target='_blank' href='https://twitter.com/joepkockelkorn'>
-          <i className='text-3xl transform transition-all group-hover:scale-125 md:text-5xl p-2 fab fa-twitter'></i>
-        </a>
-        <a className='flex items-center hover:text-yellow-500 group' target='_blank' href='https://www.linkedin.com/in/joepkockelkorn'>
-          <i className='text-3xl transform transition-all group-hover:scale-125 md:text-5xl p-2 fab fa-linkedin'></i>
-        </a>
-        <a className='flex items-center hover:text-yellow-500 group' target='_blank' href='https://medium.com/@joepkockelkorn'>
-          <i className='text-3xl transform transition-all group-hover:scale-125 md:text-5xl p-2 fab fa-medium'></i>
-        </a>
-        <a className='flex items-center hover:text-yellow-500 group' target='_blank' href='https://github.com/joepkockelkorn'>
-          <i className='text-3xl transform transition-all group-hover:scale-125 md:text-5xl p-2 fab fa-github'></i>
+      <div className='mt-8 flex flex-row justify-center flex-wrap gap-2'>
+        <a
+          className='flex items-center justify-center h-16 w-16 hover:text-orange-400 group'
+          target='_blank'
+          href='https://twitter.com/joepkockelkorn'
+        >
+          <i className='transform transition-all group-hover:scale-125 text-5xl p-2 fab fa-twitter'></i>
         </a>
         <a
-          className='flex items-center hover:text-yellow-500 group'
+          className='flex items-center justify-center h-16 w-16 hover:text-orange-400 group'
+          target='_blank'
+          href='https://www.linkedin.com/in/joepkockelkorn'
+        >
+          <i className='transform transition-all group-hover:scale-125 text-5xl p-2 fab fa-linkedin'></i>
+        </a>
+        <a
+          className='flex items-center justify-center h-16 w-16 hover:text-orange-400 group'
+          target='_blank'
+          href='https://github.com/joepkockelkorn'
+        >
+          <i className='transform transition-all group-hover:scale-125 text-5xl p-2 fab fa-github'></i>
+        </a>
+        <a
+          className='flex items-center justify-center h-16 w-16 hover:text-orange-400 group'
           target='_blank'
           href='https://stackoverflow.com/users/5475829/joep-kockelkorn'
         >
-          <i className='text-3xl transform transition-all group-hover:scale-125 md:text-5xl p-2 fab fa-stack-overflow'></i>
+          <i className='transform transition-all group-hover:scale-125 text-5xl p-2 fab fa-stack-overflow'></i>
         </a>
       </div>
     </main>
