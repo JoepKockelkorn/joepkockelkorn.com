029c45a6e53d082465661f711b2d6f29391b3f5f
Joep Kockelkorn <joepkockelkorn@gmail.com>
2023-03-16 16:45:34 +0100

    feat: use built-in tailwind support of remix

--- app/root.tsx
+++ app/root.tsx
@@ -1,9 +1,16 @@
-import type { LinksFunction, MetaFunction } from "@remix-run/cloudflare";
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import type { LinksFunction, MetaFunction } from '@remix-run/cloudflare';
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react';
 import { useShouldHydrate } from 'remix-utils';
 
 import fonts from './fonts.css';
-import styles from './styles.output.css';
+import styles from './styles.css';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
@@ -14,18 +21,21 @@ export const meta: MetaFunction = () => ({
 export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: styles },
   { rel: 'stylesheet', href: fonts },
-  { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.4.1/css/all.css' },
+  {
+    rel: 'stylesheet',
+    href: 'https://use.fontawesome.com/releases/v5.4.1/css/all.css',
+  },
 ];
 
 export default function App() {
   const shouldHydrate = useShouldHydrate();
   return (
-    <html lang='en' className='h-full flex flex-col'>
+    <html lang="en" className="h-full flex flex-col">
       <head>
         <Meta />
         <Links />
       </head>
-      <body className='h-full'>
+      <body className="h-full">
         {/* <Header /> */}
         <Outlet />
         <ScrollRestoration />
