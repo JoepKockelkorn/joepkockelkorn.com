f28bc9cc5902735bec4429127f0283b1e9fece39
Joep Kockelkorn <joepkockelkorn@gmail.com>
2023-04-16 21:49:03 +0200

    feat: add code block diff highlighting

--- app/utils/github.server.ts
+++ app/utils/github.server.ts
@@ -72,6 +72,7 @@ export async function fetchBlogPost(
 
 export function convertMarkdownToHtml(requestUrl: URL, markdown: string) {
   const renderer = new marked.Renderer();
+
   const linkRenderer = renderer.link;
   renderer.link = (href, title, text) => {
     const localLink = href?.startsWith(
@@ -85,24 +86,103 @@ export function convertMarkdownToHtml(requestUrl: URL, markdown: string) {
           `<a target="_blank" rel="noreferrer noopener nofollow" `
         );
   };
+
   const imageRenderer = renderer.image;
   renderer.image = (href, title, text) => {
     const html = imageRenderer.call(renderer, href, title, text);
     return html.replace(/^<img /, `<img loading="lazy" `);
   };
+
   renderer.heading = (text, level, raw, slugger) => {
     const id = slugger.slug(raw);
     return `<h${level} id="${id}"><a href="#${id}" class="header-link">${text}</a></h${level}>\n`;
   };
+
   hljs.getLanguage('typescript') ||
     hljs.registerLanguage('typescript', typescript);
   hljs.getLanguage('xml') || hljs.registerLanguage('xml', xml);
-  return marked(markdown, {
-    renderer,
-    highlight: (code) => hljs.highlightAuto(code).value,
-  });
+  const highlight = (code: string, _lang: string): string =>
+    hljs.highlightAuto(code).value;
+
+  renderer.code = (code, infostring, escaped) => {
+    const lang = (infostring ?? '').match(/\S*/)![0];
+    const lines = code.split('\n');
+
+    // Find diff lines
+    const diffLines = lines.reduce<
+      Array<{ rowIndex: number; type: 'addition' | 'deletion' }>
+    >((acc, line, i) => {
+      if (line.startsWith('+ ')) acc.push({ rowIndex: i, type: 'addition' });
+      if (line.startsWith('- ')) acc.push({ rowIndex: i, type: 'deletion' });
+      return acc;
+    }, []);
+
+    // Remove diff indications
+    if (diffLines.length > 0) {
+      code = lines
+        .map((line, i) =>
+          diffLines.find((dl) => dl.rowIndex === i) ? line.slice(2) : line
+        )
+        .join('\n');
+    }
+
+    // Apply syntax highlighting
+    const out = highlight(code, lang);
+    if (out != null && out !== code) {
+      escaped = true;
+      code = out;
+    }
+
+    // Add back the diff indications
+    const splittedCode = code.split('\n');
+    const aggregatedCode = splittedCode.map((line, i) => {
+      const dl = diffLines.find((dl) => dl.rowIndex === i);
+      if (!dl) return `<span class="line">${line}</span>`;
+      return `<span class="line ${
+        dl.type === 'addition' ? 'bg-green-900' : 'bg-red-900'
+      }">${dl.type === 'addition' ? '+' : '-'} ${line}</span>`;
+    });
+    code = aggregatedCode.join('\n');
+
+    code = code.replace(/\n$/, '') + '\n'; // what is this for?
+
+    const escapedCode = escaped ? code : escape(code, true);
+    return `<pre><code${
+      !lang ? '' : `class="${escape(lang)}"`
+    }>${escapedCode}</code$></pre>\n`;
+  };
+
+  return marked(markdown, { renderer, highlight });
 }
 
 function getSlugFromFilename(name: string) {
   return name.replace(/\.md$/, '');
 }
+
+/** Copied from marked (added types) */
+const escapeTest = /[&<>"']/;
+const escapeReplace = new RegExp(escapeTest.source, 'g');
+const escapeTestNoEncode = /[<>"']|&(?!(#\d{1,7}|#[Xx][a-fA-F0-9]{1,6}|\w+);)/;
+const escapeReplaceNoEncode = new RegExp(escapeTestNoEncode.source, 'g');
+const escapeReplacements: Record<string, string> = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#39;',
+};
+const getEscapeReplacement = (ch: string) => escapeReplacements[ch];
+export function escape(html: string, encode?: boolean) {
+  if (encode) {
+    if (escapeTest.test(html)) {
+      return html.replace(escapeReplace, getEscapeReplacement);
+    }
+  } else {
+    if (escapeTestNoEncode.test(html)) {
+      return html.replace(escapeReplaceNoEncode, getEscapeReplacement);
+    }
+  }
+
+  return html;
+}
+/** End copied from marked */
