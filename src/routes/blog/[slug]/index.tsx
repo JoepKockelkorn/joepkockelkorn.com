37ebad3bd6e9c8c83237c614fbf6c628a6eea750
Joep Kockelkorn <joepkockelkorn@gmail.com>
2023-09-02 14:15:44 +0200

    feat: add slug route

---
+++ src/routes/blog/[slug]/index.tsx
@@ -0,0 +1,73 @@
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+import { routeLoader$ } from '@builder.io/qwik-city';
+import invariant from 'tiny-invariant';
+
+import 'highlight.js/styles/night-owl.css';
+import { convertMarkdownToHtml, fetchBlogPost } from '~/utils/github.server';
+import { getDomainUrl } from '~/utils/domain.server';
+import { isNil } from 'remeda';
+
+export const head: DocumentHead = ({ resolveValue }) => {
+	const data = resolveValue(usePost);
+	if (isNil(data)) return {};
+
+	const {
+		meta: { title, description, draft },
+		host,
+		slug,
+	} = data;
+
+	const ogImageUrl = new URL(`${host}/og-image`);
+	ogImageUrl.searchParams.set('title', title);
+	const ogUrl = new URL(`${host}/blog/${slug}`);
+
+	return {
+		title,
+		meta: [
+			...(draft ? [{ name: 'robots', content: 'noindex' }] : []),
+			{ name: 'description', content: description },
+			{ name: 'og:url', content: ogUrl.toString() },
+			{ name: 'og:title', content: title },
+			{ name: 'og:description', content: description },
+			{ name: 'og:image', content: ogImageUrl.toString() },
+			{ name: 'twitter:title', content: title },
+			{ name: 'twitter:description', content: description },
+			{ name: 'twitter:image', content: ogImageUrl.toString() },
+			{ name: 'twitter:card', content: 'summary_large_image' },
+			{ name: 'twitter:creator', content: '@JoepKockelkorn' },
+		],
+	};
+};
+
+export const usePost = routeLoader$(async ({ query, params, url, request }) => {
+	invariant(params.slug, 'Slug is required');
+	const ref = query.get('ref') ?? undefined;
+
+	const blogPost = await fetchBlogPost(params.slug, ref);
+	if (blogPost === null) throw new Response('Not found', { status: 404 });
+	const html = convertMarkdownToHtml(url, blogPost.bodyMarkdown);
+
+	const host = getDomainUrl(request);
+
+	return { html, meta: blogPost.meta, host, slug: params.slug };
+});
+
+export default component$(() => {
+	const { html, meta } = usePost().value;
+	const { date, title } = meta;
+	const { formatted, raw } = date;
+
+	return (
+		<>
+			<time dateTime={raw} class="mt-4">
+				{formatted}
+			</time>
+			<h1 class="leading-tight font-bold text-gradient text-6xl mt-2 mb-12">{title}</h1>
+			<article
+				class="prose prose-li:my-[0.25em] prose-lg dark:prose-invert pb-[100px] prose-pre:bg-[#011627] prose-code:before:content-none prose-code:after:content-none prose-code:[&_:not(pre)]:bg-code prose-code:text-white prose-code:rounded-[4px] prose-code:[&_:not(pre)]:px-1.5 prose-code:[&_:not(pre)]:py-1 prose-code:[&_a]:underline prose-h1:leading-[1.4] max-w-none"
+				dangerouslySetInnerHTML={html}
+			/>
+		</>
+	);
+});
