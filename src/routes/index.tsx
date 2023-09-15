372c002fa197c51c65e9748dfdde48734d3a6c3a
Joep Kockelkorn <joepkockelkorn@gmail.com>
2023-09-15 14:20:41 +0200

    fix: update twitter/x icon

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -20,14 +20,26 @@ export default component$(() => {
 			<div class="font-mono text-3xl mt-4 mx-0">Full Stack Dev && Front-end fanatic</div>
 			<div class="mt-8 flex flex-row justify-center flex-wrap gap-2">
 				<a
-					class="flex items-center justify-center h-16 w-16 hover:text-primary-400 focus-visible:text-primary-400 group"
+					class="flex items-center justify-center h-16 w-16 hover:text-primary-400 focus-visible:text-primary-400 group relative"
 					target="_blank"
-					href="https://twitter.com/joepkockelkorn"
+					href="https://x.com/joepkockelkorn"
 					rel="noreferrer"
-					aria-label="Twitter @JoepKockelkorn"
+					aria-label="X (twitter) @JoepKockelkorn"
 				>
 					<svg
-						class="transform motion-safe:transition-all group-hover:scale-125 p-2 h-full w-full"
+						class="opacity-100 transform motion-safe:transition-all group-hover:opacity-0 group-focus-visible:opacity-0 h-full w-full !duration-300"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 1668.56 1221.19"
+					>
+						<path
+							fill="currentColor"
+							stroke="currentColor"
+							stroke-width="35"
+							d="m336.33 142.251 386.39 516.64-388.83 420.05h87.51l340.42-367.76 275.05 367.76h297.8l-408.13-545.7 361.92-390.99h-87.51l-313.51 338.7-253.31-338.7h-297.8zm128.69 64.46h136.81l604.13 807.76h-136.81l-604.13-807.76z"
+						/>
+					</svg>
+					<svg
+						class="absolute inset-0 opacity-0 transform motion-safe:transition-all group-hover:scale-125 group-hover:opacity-100 group-focus-visible:opacity-100 p-2 h-full w-full !duration-300"
 						viewBox="0 0 24 24"
 						xmlns="http://www.w3.org/2000/svg"
 					>
