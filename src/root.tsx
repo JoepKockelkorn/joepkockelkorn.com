38018085e2a5306964e1d380c0e43e4751900705
Joep Kockelkorn <joepkockelkorn@gmail.com>
2023-10-30 17:30:55 +0100

    feat: re-add GA4, using partytown

--- src/root.tsx
+++ src/root.tsx
@@ -1,6 +1,7 @@
 import { component$ } from '@builder.io/qwik';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
 import { RouterHead } from './components/router-head/router-head';
+import { QwikPartytown } from './components/partytown/partytown';
 
 import './styles.css';
 import './fonts.css';
@@ -10,6 +11,18 @@ export default component$(() => {
 		<QwikCityProvider>
 			<head>
 				<meta charSet="utf-8" />
+				<QwikPartytown forward={['dataLayer.push']} />
+				<script async type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=G-BS603B8LYL" />
+				<script
+					type="text/partytown"
+					dangerouslySetInnerHTML={`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-BS603B8LYL');
+					`}
+				/>
+				
 				<link rel="manifest" href="/manifest.json" />
 				<RouterHead />
 			</head>
