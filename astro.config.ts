829b90335e706959024ada2556f1db105bbc7e1c
Joep Kockelkorn <joepkockelkorn@gmail.com>
2025-02-21 23:07:38 +0100

    chore: fix vercel adapter import

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercelStatic from '@astrojs/vercel/static';
+import vercelStatic from '@astrojs/vercel';
 
 export default defineConfig({
 	output: 'static',
