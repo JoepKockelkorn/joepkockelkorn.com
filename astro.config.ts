06f433a086aa237353ffbca5e58cfe0a6053bb7d
Joep Kockelkorn <joepkockelkorn@gmail.com>
2025-02-22 13:11:09 +0100

    feat: add partytown and gtag

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from 'astro/config';
 import vercelStatic from '@astrojs/vercel';
+import partytown from '@astrojs/partytown';
+
+const logPartytown = import.meta.env.DEV;
 
 export default defineConfig({
 	output: 'static',
 	adapter: vercelStatic({}),
 	site: 'https://joepkockelkorn.com',
+	integrations: [
+		partytown({
+			config: {
+				forward: ['dataLayer.push'],
+				logCalls: logPartytown,
+				logGetters: logPartytown,
+				logSendBeaconRequests: logPartytown,
+				logImageRequests: logPartytown,
+				logMainAccess: logPartytown,
+				logScriptExecution: logPartytown,
+				logSetters: logPartytown,
+			},
+		}),
+	],
 });
