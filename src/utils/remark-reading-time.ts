5c81c02de4b4e88d19adb6c74ef93df2ca2f3659
Joep Kockelkorn <joepkockelkorn@gmail.com>
2025-02-22 20:14:38 +0100

    refactor: add util for readingTime

--- src/utils/remark-reading-time.ts
+++ src/utils/remark-reading-time.ts
@@ -1,17 +1,31 @@
 import getReadingTime from 'reading-time';
 import { toString } from 'mdast-util-to-string';
 import type { RemarkPlugin } from '@astrojs/markdown-remark';
+import { z, type RenderedContent } from 'astro:content';
 
 export const readingTimeKey = 'readingTime';
+
+const frontmatterSchema = z.object({
+	readingTime: z.string(),
+});
 
 export const remarkReadingTime: RemarkPlugin = () => {
 	return function (tree, { data }) {
 		const textOnPage = toString(tree);
 		const readingTime = getReadingTime(textOnPage);
 
-		if (!data.astro?.frontmatter) {
+		const { success: isValidFrontmatter, data: frontmatter } = frontmatterSchema.safeParse(data.astro?.frontmatter);
+		if (!isValidFrontmatter) {
 			return;
 		}
-		data.astro.frontmatter[readingTimeKey] = readingTime.text;
+		frontmatter.readingTime = readingTime.text;
 	};
 };
+
+export function getReadingTimeFromRenderedContent(renderedContent?: RenderedContent): string {
+	const { success: isValidFrontmatter, data: frontmatter } = frontmatterSchema.safeParse(renderedContent?.metadata?.frontmatter);
+	if (!isValidFrontmatter) {
+		throw new Error('Frontmatter is not valid');
+	}
+	return frontmatter.readingTime;
+}
